1cc9c8af84d60e8094998d2f73c3a0245a7515e2
yue <842828087@qq.com>
2019-03-02 22:19:59 +0800

    test11

---
+++ src/main/java/cn/yue/test/HelloWorld.java
@@ -0,0 +1,9 @@
+package cn.yue.test;
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        System.out.println("Hello yue!");
+    }
+}
